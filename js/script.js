5ac564b8201b92043e3d25a2e886d7fbeb4cd72a
Subiksha <asubiksha27@gmail.com>
2025-12-10 11:35:56 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Highlight active nav link based on current page URL
+
+const navLinks = document.querySelectorAll('nav a');
+const currentPage = window.location.pathname.split("/").pop();
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
